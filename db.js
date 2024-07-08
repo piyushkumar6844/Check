43099f7abc0e907895216f40d1fce3e85988cbde
piyushkumar6844 <piyush.kumar.che22@iitbhu.ac.in>
2024-07-08 08:59:27 +0530

    cndkc

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+//const mongoURI = "mongodb+srv://kumarpiyush6844:Piy456@cluster0.s37cbiu.mongodb.net/notering?retryWrites=true&w=majority&appName=Cluster0"
+
+// const connectToMongo = ()=>{
+//     mongoose.connect(mongoURI, ()=>{
+//         console.log("Connected to Mongo Successfully");
+//     })
+// }
+async function connectToMongo() {
+    await mongoose.connect(process.env.mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
+  }
+  
+  module.exports = connectToMongo
